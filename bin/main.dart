f28b4ec1746256b0af28a42185d2da843771ebc7
Richard <richardsatilite.dev@gmail.com>
2024-12-23 18:01:13 -0300

    Dart-async: added main.dart file

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,17 @@
+import 'package:http/http.dart';
+
 void main() {
-  print("hello world");
+  requestData();
+}
+
+requestData() {
+  String url =
+      "https://gist.githubusercontent.com/Richard-Satilite/2a58c22ad081f34ad45cfc5b87471730/raw/da939ba6dc9ec158c711ebfa8531d7928643effe/accounts.json";
+  Future<Response> futResponse = get(Uri.parse(url));
+
+  futResponse.then(
+    (Response response) {
+      print(response.body);
+    },
+  );
 }
