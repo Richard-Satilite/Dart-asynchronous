6a40989212f788616083e807fc0c79cd9797ad1d
Richard <richardsatilite.dev@gmail.com>
2024-12-23 18:43:12 -0300

    Dart-async: main.dart modded

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
   requestData();
@@ -9,9 +10,10 @@ requestData() {
       "https://gist.githubusercontent.com/Richard-Satilite/2a58c22ad081f34ad45cfc5b87471730/raw/da939ba6dc9ec158c711ebfa8531d7928643effe/accounts.json";
   Future<Response> futResponse = get(Uri.parse(url));
 
-  futResponse.then(
-    (Response response) {
-      print(response.body);
-    },
-  );
+  futResponse.then((Response response) {
+    List<dynamic> listAccounts = json.decode(response.body);
+    Map<String, dynamic> mapCarla =
+        listAccounts.firstWhere((element) => element["name"] == "Carla");
+    print(mapCarla["balance"]);
+  });
 }
