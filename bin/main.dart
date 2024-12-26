88aa3330f76638fa57c42c1695d37aff651f054a
Richard <richardsatilite.dev@gmail.com>
2024-12-26 14:04:40 -0300

    Dart-async: Added comment in main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,4 @@
-import 'package:dart_asynchronous/api_key.dart';
+import 'package:dart_asynchronous/api_key.dart'; //THIS PACKAGE IS ONLY AVAIABLE FOR OWNER
 import 'package:http/http.dart';
 import 'dart:convert';
 
