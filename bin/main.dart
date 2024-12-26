0bf0babdc1ae7fe1dc18724517b2d4e9486da654
Richard <richardsatilite.dev@gmail.com>
2024-12-26 14:02:22 -0300

    Dart-async: Updated main.dart to support posting content to Gist.

--- bin/main.dart
+++ bin/main.dart
@@ -1,32 +1,35 @@
+import 'package:dart_asynchronous/api_key.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
 void main() {
-  requestData();
-
-  requestAsync();
+  sendDataAsync(
+      {"id": "NEWID", "name": "async", "lastName": "test", "balance": 400.00});
 }
 
-requestData() {
-  String url =
-      "https://gist.githubusercontent.com/Richard-Satilite/2a58c22ad081f34ad45cfc5b87471730/raw/da939ba6dc9ec158c711ebfa8531d7928643effe/accounts.json";
-  Future<Response> futResponse = get(Uri.parse(url));
-
-  futResponse.then((Response response) {
-    List<dynamic> listAccounts = json.decode(response.body);
-    Map<String, dynamic> mapCarla =
-        listAccounts.firstWhere((element) => element["name"] == "Carla");
-    print(mapCarla["balance"]);
-  });
-}
-
-requestAsync() async {
+Future<List<dynamic>> requestAsync() async {
   String url =
       "https://gist.githubusercontent.com/Richard-Satilite/2a58c22ad081f34ad45cfc5b87471730/raw/da939ba6dc9ec158c711ebfa8531d7928643effe/accounts.json";
 
   Response response = await get(Uri.parse(url));
 
-  print(json.decode(response.body)[0]);
+  return json.decode(response.body);
+}
 
-  print("Último evento");
+sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+
+  String url = "https://api.github.com/gists/2a58c22ad081f34ad45cfc5b87471730";
+  Response response = await post(Uri.parse(url),
+      headers: {"Authorization": "Bearer $gistKey"},
+      body: json.encode({
+        "description": "accounts.json",
+        "public": true,
+        "files": {
+          "accounts.json": {"content": content}
+        }
+      }));
+  print(response.statusCode);
 }
