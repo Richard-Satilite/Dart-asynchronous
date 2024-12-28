4eee4747ba5aa23d45c0259284b680b928f68db7
Richard <richardsatilite.dev@gmail.com>
2024-12-28 15:23:36 -0300

    Dart-async: Added streamController in main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,17 @@
+import 'dart:async';
+
 import 'package:dart_asynchronous/api_key.dart'; //THIS PACKAGE IS ONLY AVAIABLE FOR THE OWNER
 import 'package:http/http.dart';
 import 'dart:convert';
 
+StreamController<String> streamController = StreamController<String>();
+
 void main() {
+  StreamSubscription subscription = streamController.stream.listen(
+    (String info) {
+      print(info);
+    },
+  );
   sendDataAsync(
       {"id": "NEWID", "name": "async", "lastName": "test", "balance": 400.00});
 }
@@ -12,7 +21,7 @@ Future<List<dynamic>> requestAsync() async {
       "https://gist.githubusercontent.com/Richard-Satilite/2a58c22ad081f34ad45cfc5b87471730/raw/da939ba6dc9ec158c711ebfa8531d7928643effe/accounts.json";
 
   Response response = await get(Uri.parse(url));
-
+  streamController.add("${DateTime.now()} | Requisição de leitura");
   return json.decode(response.body);
 }
 
@@ -31,5 +40,11 @@ sendDataAsync(Map<String, dynamic> mapAccount) async {
           "accounts.json": {"content": content}
         }
       }));
-  print(response.statusCode);
+  if (response.statusCode.toString()[0] == '2') {
+    streamController.add(
+        "${DateTime.now()} | Requisição de adiçao bem sucedida (${mapAccount["name"]})");
+  } else {
+    streamController
+        .add("${DateTime.now()} | Falha na requisição (${mapAccount["name"]})");
+  }
 }
